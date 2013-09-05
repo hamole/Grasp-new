b8dd10bb1d275530ff3005cd97d92211cdbdd25f
Hamish Graham <graham.hamish@gmail.com>
2013-09-05 22:30:57 +1000

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GraspNew::Application.config.secret_token = 'fdbfc8bb1e4aa778256b2929f0d362f32682b0d12ad11c15bb9f7876e4df77e111cdfc741b4ed61c43d7f5908abe37a65ebcbeb382740e65820c92ae0c789b52'
